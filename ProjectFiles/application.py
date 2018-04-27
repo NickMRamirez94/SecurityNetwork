d2c8a366569afdbe9e5ec123e1c4183cd06abde5
NickMRamirez94 <bcgnick@gmail.com>
2018-04-26 18:08:03 -0700

    updating application.py for WelcomeClient

--- ProjectFiles/application.py
+++ ProjectFiles/application.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 from cryptography.fernet import Fernet
 
-cnx = mysql.connector.connect(user='root', password='smackdown2', database='security')
+cnx = mysql.connector.connect(user='root', password='password', database='security')
 
 def Login():
     username = raw_input("Username: ")
